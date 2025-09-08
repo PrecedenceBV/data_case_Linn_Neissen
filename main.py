4dc56651a254f004d9f271dead5f75da3895019d
pimsteijns <pim.steijns@precedence.nl>
2025-09-08 13:14:42 +0200

    Uiteindelijke code van Linn -> geen juiste uitkomst. Vragen aan chatGPT bijgevoegd

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
-# This is a sample Python script.
+import pandas as pd
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+nps_scores = pd.read_csv("/Users/precedenceintern/PycharmProjects/data_case_Linn_Neissen/data-case/nps_scores.csv", sep=";")
 
+print(nps_scores)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+total = nps_scores['Recommendation_score'].sum()
+print(total)
 
+promoters = nps_scores['Recommendation_score'].isin([9, 10]).sum()
+promoters_perc = promoters / total * 100
+print(promoters_perc)
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+detracters = nps_scores['Recommendation_score'].isin([0, 1, 2, 3, 4, 5, 6]).sum()
+detracters_perc = detracters / total * 100
+print(detracters)
+print(detracters_perc)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+#result = promoters_perc.groupby('Journey_Type')['Recommendation_score'].isin([9,10]).mean() * 100
+
+result = nps_scores.groupby('Journey_Type').apply(
+    lambda x: x['Recommendation_score'].isin([9, 10]).mean() * 100
+).reset_index(name='Percentage_9_or_10')
+
+nps = promoters_perc - detracters_perc
+
